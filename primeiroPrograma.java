9d25138a80e722c09539c60eff57c080b7902c98
lu-lui <lpneutzling@gmail.com>
2026-05-18 21:47:37 -0300

    hello world

---
+++ primeiroPrograma.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author luisa
+ */
+public class primeiroPrograma {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
